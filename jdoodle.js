8f65287e742ddbca3d75b4a6cc60ebc167a18f7c
AbdelRahman Salah Makhlouf Mohamed <abdomagik80@gmail.com>
2021-09-18 12:55:12 +0200

    Add files via upload

---
+++ jdoodle.js
@@ -0,0 +1,21 @@
+/*Excuse me i write some weird things it's just practise that's all*/
+
+var hunter = [["mister","hunterX","mistarV"],["gero","kane","alo"]]
+
+let feroza =0  /*here it's the 1st row*/
+let heroza =1  /*here is the 2ed row*/
+
+var uga = hunter[feroza][1]
+var gua = hunter[feroza][2]
+var hes = hunter[heroza][2]
+var fsd = hunter[heroza][0]
+
+console.log(uga)
+console.log(gua)
+console.log(hes)
+console.log(fsd)
+
+hunter[feroza][0] = "trainer"
+console.log([hunter[feroza][0]])
+
+console.log(hunter)
---
+++ jdoodle.swift
@@ -0,0 +1,21 @@
+var studentperPets = [1,2,3,5,4,7,6,8,9,7,9,5,1,2,5,0,0,2,0,1]
+
+var numOfstudent = studentperPets.count
+
+//summ of students/num of them//
+
+print(studentperPets[3])
+//print(studentperPets[200])
+print(numOfstudent)
+
+var x=0
+
+for SUM_pets in studentperPets {
+    x=x+(SUM_pets)
+}
+
+print(x)
+
+var average = x/numOfstudent
+
+print(average)
