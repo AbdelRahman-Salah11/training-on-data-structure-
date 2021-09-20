88c8fa7cf9daf5f5d078e00c50be2dd09b26b77f
AbdelRahman Salah Makhlouf Mohamed <abdomagik80@gmail.com>
2021-09-20 23:11:17 +0200

    Add files via upload

---
+++ #here is practise on the key value and h.py
@@ -0,0 +1,17 @@
+#here is practise on the key value and hash table
+#key:value
+
+
+#the first point is to intiate and create associative array or (dictionary)
+Egypt_is_wonderful = {}
+
+# dictionary_name[key]=value "
+
+Egypt_is_wonderful["cairo"] = "Giza"
+Egypt_is_wonderful["Suiz"]  = "Canal"
+Egypt_is_wonderful["Sinai"] = "Hergada"
+
+
+city=str(input("what's your favorite city and we'll tell you it's center"))
+
+print("the center of govrenment you choose is",Egypt_is_wonderful[city])
